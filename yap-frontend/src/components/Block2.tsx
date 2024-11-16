22712689e30142ee58f415d2bd92bf030447d7a0
Haimantika Mitra <haimantikamitra@gmail.com>
2024-11-16 14:29:23 +0530

    updated bg and text colours

--- yap-frontend/src/components/Block2.tsx
+++ yap-frontend/src/components/Block2.tsx
@@ -1,6 +1,6 @@
 export default function Block2() {
     return (
-      <div className="w-full max-w-2xl text-center mb-6 p-8 text-[#28102E]">
+      <div className="w-full max-w-2xl text-center mb-6 p-8 text-[#fafafa]">
         <p className="mb-4 font-geist text-lg ">
         Transform your typical millennial and boomer sentences into 
 Gen Z and Gen Alpha lingo with just one click 💅
